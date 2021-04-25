ab3e4777febba51903c62e8e3ac0958c7ee3fb59
FlutLab.io <>
2021-04-25 23:01:19 +0000

    Autosave

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,7 +4,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white(),
+      color: Colors.white,
     );
   }
 }
