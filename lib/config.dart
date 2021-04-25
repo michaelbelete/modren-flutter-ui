e85a8af519fcc4849d8187fa2a60d103a4130b9e
FlutLab.io <>
2021-04-25 22:53:36 +0000

    Autosave

---
+++ lib/config.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Color primaryGreen = Color(0xff416d6d);
+List<BoxShadow> shadowList = [
+  BoxShadow(color: Colors.grey[300], blurRadius: 30, offset: Offset(0, 10))
+];
+
+List<Map> categories = [
+  {
+    'name': 'Cats',
+    'iconPath': 'images/cat.png'
+  },
+  {
+    'name': 'Dogs',
+    'iconPath': 'images/dog.png'
+  },
+  {
+    'name': 'Bunnies',
+    'iconPath': 'images/rabbit.png'
+  },
+  {
+    'name': 'Parrots',
+    'iconPath': 'images/parrot.png'
+  },
+  {
+    'name': 'Horses',
+    'iconPath': 'images/horse.png'
+  }
+];
