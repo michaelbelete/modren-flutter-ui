d782266bc424364d68e8d5dbf9f4656053bf8da1
FlutLab.io <>
2021-04-25 23:05:43 +0000

    Autosave

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-    );
+    return AnimatedContainer(
+        duration: Duration(
+          milliseconds: 250,
+        ),
+        color: Colors.white,
+        child: Column(children: [
+          SizedBox(
+            height: 50,
+          ),
+          Row(children: [
+            IconButton(icon: Icon(Icons.menu), onPressed: () {})
+          ])
+        ]));
   }
 }
